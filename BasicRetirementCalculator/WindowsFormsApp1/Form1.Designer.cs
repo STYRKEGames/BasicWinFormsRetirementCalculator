18e61dcc6bc93fd0be9c23d5eb77717075ed272b
STYRKEGames <styrkegames@gmail.com>
2019-03-11 22:10:15 -0700

    Added Ability to Adjust for Inflation
    You can now calculate inflation with the basic retirement calculator. =)

--- BasicRetirementCalculator/WindowsFormsApp1/Form1.Designer.cs
+++ BasicRetirementCalculator/WindowsFormsApp1/Form1.Designer.cs
@@ -34,30 +34,34 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.rtbTotalToRetire = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtInflationRate = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 114);
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(112, 79);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(221, 23);
+            this.button1.Size = new System.Drawing.Size(134, 44);
             this.button1.TabIndex = 0;
             this.button1.Text = "Calculate Retirement Savings Needed";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // txtLivingExpense
             // 
             this.txtLivingExpense.Location = new System.Drawing.Point(15, 25);
             this.txtLivingExpense.Name = "txtLivingExpense";
-            this.txtLivingExpense.Size = new System.Drawing.Size(100, 20);
+            this.txtLivingExpense.Size = new System.Drawing.Size(169, 20);
             this.txtLivingExpense.TabIndex = 1;
             // 
             // txtYearsToRetire
             // 
-            this.txtYearsToRetire.Location = new System.Drawing.Point(15, 88);
+            this.txtYearsToRetire.Location = new System.Drawing.Point(15, 64);
             this.txtYearsToRetire.Name = "txtYearsToRetire";
-            this.txtYearsToRetire.Size = new System.Drawing.Size(100, 20);
+            this.txtYearsToRetire.Size = new System.Drawing.Size(78, 20);
             this.txtYearsToRetire.TabIndex = 2;
             // 
             // label1
@@ -72,7 +76,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 60);
+            this.label2.Location = new System.Drawing.Point(12, 48);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(234, 13);
             this.label2.TabIndex = 4;
@@ -80,25 +84,45 @@
             // 
             // rtbTotalToRetire
             // 
-            this.rtbTotalToRetire.Location = new System.Drawing.Point(261, 9);
+            this.rtbTotalToRetire.Location = new System.Drawing.Point(252, 9);
             this.rtbTotalToRetire.Name = "rtbTotalToRetire";
-            this.rtbTotalToRetire.Size = new System.Drawing.Size(203, 99);
+            this.rtbTotalToRetire.Size = new System.Drawing.Size(185, 120);
             this.rtbTotalToRetire.TabIndex = 5;
             this.rtbTotalToRetire.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Inflation Rate %";
+            // 
+            // txtInflationRate
+            // 
+            this.txtInflationRate.Location = new System.Drawing.Point(15, 103);
+            this.txtInflationRate.Name = "txtInflationRate";
+            this.txtInflationRate.Size = new System.Drawing.Size(78, 20);
+            this.txtInflationRate.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(510, 155);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(449, 138);
+            this.Controls.Add(this.txtInflationRate);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.rtbTotalToRetire);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtYearsToRetire);
             this.Controls.Add(this.txtLivingExpense);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Basic Retirement Calculator";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -112,6 +136,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.RichTextBox rtbTotalToRetire;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtInflationRate;
     }
 }
 
--- BasicRetirementCalculator/WindowsFormsApp1/Form1.cs
+++ BasicRetirementCalculator/WindowsFormsApp1/Form1.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,25 +26,33 @@ namespace WindowsFormsApp1
         {
             // Variables
             decimal livingExpense;
-            decimal yearsToRetire;
+            int yearsToRetire;
+            float inflationRatePercent;
 
             // Get Input from Users through the textbox
             livingExpense = decimal.Parse(txtLivingExpense.Text);
-            yearsToRetire = decimal.Parse(txtYearsToRetire.Text);
+            yearsToRetire = int.Parse(txtYearsToRetire.Text);
+            inflationRatePercent = float.Parse(txtInflationRate.Text) / 100;
 
             // Calculate values
-            decimal totalValue = Convert.ToDecimal(livingExpense) * Convert.ToDecimal(yearsToRetire);
+            decimal inflationRateTotal = Convert.ToDecimal(inflationRatePercent) * livingExpense;
+            decimal inflationRateYear = inflationRateTotal * Convert.ToDecimal(yearsToRetire);
+            decimal inflationTotal = livingExpense + inflationRateYear * yearsToRetire;
+
+            decimal totalValue = inflationTotal * yearsToRetire;
+     
 
             // return Values to user
-            rtbTotalToRetire.Text = "If your total cost of living expense is " + livingExpense + " annually." + 
-                                    " You should save a total of " + totalValue + "." +
-                                    " To retire for the next " + yearsToRetire + " Years";
+            rtbTotalToRetire.Text = "If your cost of living expense is kept the same at $" + livingExpense + " annually." +
+                                    "\nWith a inflation rate at " + inflationRatePercent +
+                                    "%, the total you should try to save is $" + totalValue + "." +
+                                    " To retire for " + yearsToRetire + " Years";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             RetirementCalculations();
-
+            
         }
 #endregion
     }
